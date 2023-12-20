4040ba8682bd23bf1bcd5ffea2d8aa0b843b76f0
KHARERISHABH <rishabkhare41@gmail.com>
2023-12-20 20:31:56 +0530

    destroying the component manually and unsubscribing it

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit{
   user: {id: number, name: string};
+  paramsSubscription: Subscription
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,10 +18,14 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
-    this.route.params.subscribe((param: Params) => {
+    this.paramsSubscription = this.route.params.subscribe((param: Params) => {
       this.user.id = param['id']
       this.user.name = param['name']
     })
+  }
+
+  ngOnDestroy() {
+      this.paramsSubscription.unsubscribe()
   }
 
 }
